de5f4baad50c34030a9d814e7de02c473fa6230f
Ted José <ted@ted.net.br>
2024-09-22 06:11:38 -0300

    Instalação do express

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const WebSocket = require('ws');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 // Servir arquivos HTML na pasta public
 app.use(express.static(path.join(__dirname, 'public')));
